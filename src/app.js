424765fd2064dc4af2da58d293e24510a36ee5de
Nikolas <nikolas@conectra.net.br>
2020-06-23 13:58:06 -0300

    [ADD] rota post e get

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,15 +11,47 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return repositories;
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+
+  const { title, url, techs } = request.body;
+
+  const repositorie = { 
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0 
+  }
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs} = request.params;
+  const { id } = request.params
+
+  const repositorieIndex = repositories.findIndex((repositorie) => (repositorie.id === id));
+ 
+  if(repositorieIndex < 0){
+    return response.status(400).json({error: "Repositorie not found!"});
+  }
+
+  const likes = repositories[repositorieIndex].like;
+
+  repositories[repositorieIndex] = {
+    id,
+    title,
+    url,
+    techs,
+    likes,
+  }
+
+  return response.json(repositories[repositorieIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
